d5b3395074f0ea8e582e39dc3f3884c076d6dfae
Kamal Aghayev <agayev169@gmail.com>
2020-11-22 22:27:47 +0400

    Added Zoom function to Mandelbrot

--- mandelbrot/README.md
+++ mandelbrot/README.md
@@ -2,8 +2,11 @@
 
 Implementation of the Mandelbrot set in golang.
 
-The result of the algorithm with `setBeginX = -2.5`, `setBeginY = -1.0` and `stepSize = 0.002` for an 1920x1080 image:
+The result of the `mandelbrot.Mandelbrot` function with `setBeginX = -2.5`, `setBeginY = -1.0` and `stepSize = 0.002` for a 1920x1080 image:
 ![Result of the algorithm](imgs/img.png)
+
+The reslt of the `mandelbrot.Zoom` function with `setCenterX = -0.761574`, `setCenterY = -0.0847596`, `stepSizeBegin = 0.002`, `stepCoef = 0.9` for a 1920x1080 image with 120 frames:
+![Result of the algorithm](imgs/img.gif)
 
 ## Installation
 `go get github.com/agayev169/golang_examples/mandelbrot`
@@ -29,5 +32,26 @@ func main() {
     
 	png.Encode(f, img)
 }
+```
 
+```
+package main
+
+import (
+	"github.com/agayev169/golang_examples/mandelbrot"
+	"image/gif"
+	"os"
+)
+
+func main() {
+	img := mandelbrot.Zoom(1920, 1080, -0.761574, -0.0847596, 0.002, 0.9, 120)
+
+	f, err := os.Create("img.gif")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	gif.EncodeAll(f, img)
+}
 ```
--- mandelbrot/mandelbrot.go
+++ mandelbrot/mandelbrot.go
@@ -2,6 +2,9 @@ package mandelbrot
 
 import (
 	"image"
+	"image/color/palette"
+	"image/draw"
+	"image/gif"
 	"math"
 )
 
@@ -11,7 +14,7 @@ func Mandelbrot(imgWidth, imgHeight int, setBeginX, setBeginY, stepSize float64)
 
 	for y, setCurrentY := 0, setBeginY; y < imgHeight; y, setCurrentY = y+1, setCurrentY+stepSize {
 		for x, setCurrentX := 0, setBeginX; x < imgWidth; x, setCurrentX = x+1, setCurrentX+stepSize {
-			value := mandelbrotFunction(complex(setCurrentX, setCurrentY), 250, 16.0)
+			value := mandelbrotFunction(complex(setCurrentX, setCurrentY), 255, 16.0)
 			img.Set(x, y, value)
 		}
 	}
@@ -19,9 +22,51 @@ func Mandelbrot(imgWidth, imgHeight int, setBeginX, setBeginY, stepSize float64)
 	return &img
 }
 
+// Zoom - returns a GIF of zooming in the Mandelbrot set
+func Zoom(imgWidth, imgHeight int, setCenterX, setCenterY, stepSizeBegin, stepCoef float64,
+	frameCount uint) *gif.GIF {
+	steps := make([]float64, frameCount, frameCount)
+	for i := uint(0); i < frameCount; i++ {
+		steps[i] = stepSizeBegin * math.Pow(stepCoef, float64(i))
+	}
+
+	res := &gif.GIF{}
+
+	c := make(chan struct {
+		Index uint
+		Frame *image.Paletted
+	}, 100)
+
+	for i := uint(0); i < frameCount; i++ {
+		setBeginX := setCenterX - float64(imgWidth/2)*steps[i]
+		setBeginY := setCenterY - float64(imgHeight/2)*steps[i]
+		go func(i uint, setBeginX, setBeginY float64, c chan<- struct {
+			Index uint
+			Frame *image.Paletted
+		}) {
+			frame := Mandelbrot(imgWidth, imgHeight, setBeginX, setBeginY, steps[i])
+			paletted := image.NewPaletted(frame.Bounds(), palette.Plan9)
+			draw.Draw(paletted, paletted.Rect, frame, paletted.Rect.Min, draw.Over)
+			c <- struct {
+				Index uint
+				Frame *image.Paletted
+			}{Index: i, Frame: paletted}
+		}(i, setBeginX, setBeginY, c)
+		res.Image = append(res.Image, nil)
+		res.Delay = append(res.Delay, 0)
+	}
+
+	for i := uint(0); i < frameCount; i++ {
+		frameInfo := <-c
+		res.Image[frameInfo.Index] = frameInfo.Frame
+	}
+
+	return res
+}
+
 // mandelbrotFunction - returns the pixel value for (x, y) position
 func mandelbrotFunction(c complex128, steps int, threshold float64) uint8 {
-	res := uint8(255)
+	res := uint8(steps)
 	var z complex128 = 0
 
 	for i := 0; i < steps; i++ {
